680ec15703bbaedd050b70f218e819d7d622918b
is-you <57096827+is-you@users.noreply.github.com>
2023-06-06 16:30:58 +0300

    Add files via upload
    color scheme

--- src/assets/js/App.js
+++ src/assets/js/App.js
@@ -1,18 +1,33 @@
 /** app init class **/
 class App {
-  /** create app **/
-  constructor() {
-    this.init();
-  }
+	/** create app **/
+	constructor() {
+		this.init();
+	}
 
-  /** init other modules **/
-  init() {
-    window.addEventListener('load', ()=>{
-      const tg = window.Telegram.WebApp;
-      console.log(tg.colorScheme, tg.headerColor, tg.backgroundColor, tg.colorScheme, tg.themeParams );
-      createForm();
-    });
-  }
+	/** init other modules **/
+	init() {
+		window.addEventListener('load', () => {
+			const tg = window.Telegram.WebApp;
+
+			if(tg.headerColor !== null){
+				setColorScheme(tg.themeParams);
+			}
+			
+			tg.MainButton.show();
+
+			console.log(tg.colorScheme, tg.headerColor, tg.backgroundColor, tg.themeParams);
+			createForm();
+		});
+	}
+}
+
+function setColorScheme(scheme) {
+	document.documentElement.style.setProperty("--color-form-background", scheme.bg_color);
+	document.documentElement.style.setProperty("--color-background", scheme.secondary_bg_color);
+	document.documentElement.style.setProperty("--color-decor", scheme.button_color);
+	document.documentElement.style.setProperty("--color-button-text", scheme.button_text_color);
+	document.documentElement.style.setProperty("--color-text", scheme.text_color);
 }
 
 new App();
